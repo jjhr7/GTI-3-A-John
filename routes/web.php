7520915dcd2d83ad94d945c9e362561cf1cd9e9a
John <jonnyhernandezramirez7@gmail.com>
2021-10-15 16:01:05 +0200

    Cambios en la plantilla
    Se han hecho pequeños cambios en la plantilla para personalizarla un poco

--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,7 @@ use App\Http\Controllers\PermissionController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/', function () { return view('home'); });
+Route::get('/', [LoginController::class,'showLoginForm']);
 
 
 Route::get('login', [LoginController::class,'showLoginForm'])->name('login');
