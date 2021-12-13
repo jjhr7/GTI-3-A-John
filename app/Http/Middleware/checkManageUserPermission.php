bfec1b2ceaff5116ff94b3498b322552f36d0152
Belén Grande <bgralop@epsg.upv.es>
2021-12-13 15:29:26 +0100

    Manage cosas v1

---
+++ app/Http/Middleware/checkManageUserPermission.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class checkManageUserPermission
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+        $PermisionsUser = auth()->user()->information->role->permissions;
+
+        dd($PermisionsUser);
+
+        $permisoDenegado = true;
+
+        foreach ($PermisionsUser as $permission){
+            if($permission->name == 'Manage user'){
+                $permisoDenegado = false;
+            }
+        }
+
+        if($permisoDenegado){
+            abort(403, 'Unauthorized action');
+        }else{
+            return $next($request);
+        }
+
+    }
+}
