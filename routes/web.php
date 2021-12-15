d8caa346d58e05ff66e6aeed8bc29d4d851bd73f
John <jonnyhernandezramirez7@gmail.com>
2021-12-15 22:43:16 +0100

    Maquetación gráficas

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,7 @@ use App\Http\Controllers\ReadController;
 use App\Http\Controllers\GasGuideController;
 use App\Http\Controllers\TownController;
 use \App\Http\Controllers\DeviceController;
+use App\Http\Controllers\HealthyTownController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -109,7 +110,12 @@ Route::group(['middleware' => 'auth'], function(){
     Route::get('/devices/get-list', [DeviceController::class, 'getDeviceList']);
     Route::get('/devices/{id}', [DeviceController::class, 'show']);
 
-
+    // Rutas para gestionar healthyTowns
+    Route::get('/healthytowns', [HealthyTownController::class,'index']);
+    Route::get('/healthytowns/get-list', [HealthyTownController::class,'getHealthyTownList']);
+    Route::post('/healthytown/create', [HealthyTownController::class,'create']);
+    Route::get('/healthytown/createForm', [HealthyTownController::class,'createForm']);
+    Route::get('/healthytown/delete/{id}', [HealthyTownController::class,'delete']);
 
 	// permission examples
     Route::get('/permission-example', function () {
@@ -171,6 +177,11 @@ Route::group(['middleware' => 'auth'], function(){
     });
     Route::get('/towns',[TownController::class,'index']);
     Route::get('/town/get-list', [TownController::class,'getTownList']);
+    Route::post('/town/create', [TownController::class,'create']);
+    Route::get('/town/createForm', [TownController::class, 'createForm']);
+    Route::get('/town/{id}', [TownController::class, 'edit']);
+    Route::post('/town/update', [TownController::class,'update']);
+    Route::get('/town/delete/{id}', [TownController::class,'delete']);
 
 });
 
