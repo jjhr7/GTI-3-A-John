dce0599a8688dc6c99dfea11ca7402201c2e9210
Leire <lvilmar1@epsg.upv.es>
2021-12-17 15:28:27 +0100

    Header colour
    Cambio del color de fondo del header

--- resources/views/gas-guide/gas-guide.blade.php
+++ resources/views/gas-guide/gas-guide.blade.php
@@ -12,6 +12,18 @@
 
     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800" rel="stylesheet">
 
+    <!-- CSS here -->
+    <link rel="stylesheet" href="css/bootstrap.min.css">
+    <link rel="stylesheet" href="css/animate.min.css">
+    <link rel="stylesheet" href="css/magnific-popup.css">
+    <link rel="stylesheet" href="fontawesome/css/all.min.css">
+    <link rel="stylesheet" href="css/dripicons.css">
+    <link rel="stylesheet" href="css/slick.css">
+    <link rel="stylesheet" href="css/default.css">
+    <link rel="stylesheet" href="css/swiper.css">
+    <link rel="stylesheet" href="css/style-landing.css">
+    <link rel="stylesheet" href="css/responsive.css">
+
     <link rel="stylesheet" href="{{ asset('plugins/bootstrap/dist/css/bootstrap.min.css') }}">
     <link rel="stylesheet" href="path/to/font-awesome/css/font-awesome.min.css">
     <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
@@ -21,10 +33,52 @@
     <link rel="stylesheet" href="{{ asset('dist/css/theme.min.css') }}">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
     <link rel="stylesheet" href="{{ asset('css/gas-guide.css') }}">
+
     <script src="{{ asset('src/js/vendor/modernizr-2.8.3.min.js') }}"></script>
 </head>
 
 <body>
+
+<!-- header -->
+<header class="header-area header-colour">
+    <div id="header-sticky" class="menu-area">
+        <div class="container">
+            <div class="second-menu">
+                <div class="row align-items-center">
+                    <div class="col-xl-2 col-lg-2">
+                        <div class="logo">
+                            <a href="index.html"><img src="img/logo/logo_largo_blanco.png" alt="logo" width="230" height="auto"></a>
+                        </div>
+                    </div>
+                    <div class="col-xl-8 col-lg-9">
+                        <div class="responsive"><i class="icon dripicons-align-right"></i></div>
+                        <div class="main-menu text-right text-xl-right">
+                            <nav id="mobile-menu">
+                                <ul>
+                                    <li class="has-sub">
+                                        <a href="#parallax">Inicio</a>
+                                    </li>
+                                    <li><a href="#features">Funcionalidades</a></li>
+                                    <li><a href="#screen">Pantallas</a></li>
+                                    <li><a href="/guia">Información adicional</a></li>
+                                    <li><a href="#blog">Blog</a></li>
+                                    <li><a href="#contact">Contacto</a></li>
+                                </ul>
+                            </nav>
+                        </div>
+                    </div>
+                    <div class="col-xl-2 text-right d-none d-xl-block">
+                        <div class="header-btn second-header-btn d-xl-flex">
+                            <a href="/login" class="btn">Iniciar sesión</a>
+                            <a href="/register" class="btn">Registro</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</header>
+<!-- end header -->
 <!--[if lt IE 8]>
 <p class="browserupgrade">You are using an <strong>outdated</strong> browser. Please <a href="http://browsehappy.com/">upgrade your browser</a> to improve your experience.</p>
 <![endif]-->
@@ -58,7 +112,7 @@
                         <div class="col boxIcono"><a href="#"><img class="box-gas5" id="arsenico" onclick="datosArsenico()" src="{{asset('img/gas-guide-img/arsenico.png')}}"></a></div>
                     </div>
                     <div class="row mb-12">
-                        <div class="col-5 boxIcono"><a href="#"><img class="imgGas" id="imagenGas" src="{{asset('img/gas-guide-img/humo.png')}}"></a></div>
+                        <div class="col-5 boxIcono"><a href="#"><img class="imgGas transicionImg" id="imagenGas" src="{{asset('img/gas-guide-img/humo.png')}}"></a></div>
                         <div class="col-7">
                             <div class="row md-12">
                                     <div class="col-md boxInfo">
@@ -93,7 +147,104 @@
         </div>
     </div>
 </div>
+</div>
 
+    <!-- footer -->
+    <footer class="footer-bg footer-p pt-60" style="background-image: url(img/bg/f-bg.png); background-position: center top; background-size: auto;background-repeat: no-repeat;">
+
+        <div class="footer-top">
+            <div class="container">
+                <div class="row justify-content-between">
+                    <div class="col-xl-3 col-lg-3 col-sm-6">
+                        <div class="footer-widget mb-30">
+                            <div class="logo mt-15 mb-15">
+                                <a href="#"><img src="img/logo/logo_largo_blanco.png" alt="logo" width="200px" height="auto"></a>
+                            </div>
+                            <div class="footer-text mb-20">
+                                <p>Tecnologías Interactivas EPSG</p>
+                            </div>
+                            <div class="footer-social">
+                                <a href="#"><i class="fab fa-facebook-f"></i></a>
+                                <a href="#"><i class="fab fa-twitter"></i></a>
+                                <a href="#"><i class="fab fa-instagram"></i></a>
+                                <a href="#"><i class="fab fa-google-plus-g"></i></a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-lg-3 col-sm-6">
+                        <div class="footer-widget mb-30">
+                            <div class="f-widget-title">
+                                <h5>Nuestra compañía</h5>
+                            </div>
+                            <div class="footer-link">
+                                <ul>
+                                    <li><a href="#">Sobre nosotros</a></li>
+                                    <li><a href="#">Términos y condiciones</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-2 col-lg-3 col-sm-6">
+                        <div class="footer-widget mb-30">
+                            <div class="f-widget-title">
+                                <h5>Enlaces de interés</h5>
+                            </div>
+                            <div class="footer-link">
+                                <ul>
+                                    <li><a href="#">Inicio</a></li>
+                                    <li><a href="#">Quienes somos</a></li>
+                                    <li><a href="#">Nuestras funcionalidades</a></li>
+                                    <li><a href="#">Nuestra app</a></li>
+                                    <li><a href="#">Noticias</a></li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-xl-3 col-lg-3 col-sm-6">
+                        <div class="footer-widget mb-30">
+                            <div class="f-widget-title">
+                                <h5>Contáctanos</h5>
+                            </div>
+                            <div class="footer-link">
+                                <div class="f-contact">
+                                    <ul>
+                                        <li>
+                                            <i class="icon dripicons-phone"></i>
+                                            <span>6786786789<br>+34 6786786789</span>
+                                        </li>
+                                        <li>
+                                            <i class="icon dripicons-mail"></i>
+                                            <span><a href="mailto:info@example.com">marselosa@epsg.upv.es</a><br><a href="mailto:sale@example.com">marselocontact@epsg.upv.es</a></span>
+                                        </li>
+                                        <li>
+                                            <i class="fal fa-map-marker-alt"></i>
+                                            <span>UPV Campus de Gandía<br>Carrer del Paraninf, 1, Playa de Gandía</span>
+                                        </li>
+                                    </ul>
+
+                                </div>
+
+
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="copyright-wrap text-center">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                        <div class="copyright-text">
+                            <p>&copy; 2021 @ Marselo SA.</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+    <!-- footer-end -->
+    <!-- Start Scripts -->
 <script type="text/javascript">
     function datosCO2() {
         document.getElementById("nombreGas").innerText = "{{$gases[0]->name}}";
