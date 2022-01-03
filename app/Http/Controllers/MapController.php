2bd36ed0bf99c968c9f1bbe52e8c56da262de2a6
John <jonnyhernandezramirez7@gmail.com>
2022-01-03 17:51:19 +0100

    Creación entidad Map
    Se ha creado los archivos correspondientes para la lógica de mapas

---
+++ app/Http/Controllers/MapController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreMapRequest;
+use App\Http\Requests\UpdateMapRequest;
+use App\Models\Map;
+
+class MapController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreMapRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreMapRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Map  $map
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Map $map)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Map  $map
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Map $map)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateMapRequest  $request
+     * @param  \App\Models\Map  $map
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateMapRequest $request, Map $map)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Map  $map
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Map $map)
+    {
+        //
+    }
+}
