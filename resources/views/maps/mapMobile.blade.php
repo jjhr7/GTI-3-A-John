81255570319b482a8157d0eea966cc989e71790b
Belén Grande <bgralop@epsg.upv.es>
2022-01-08 12:42:59 +0100

    Mapa movil ya va

--- resources/views/maps/mapMobile.blade.php
+++ resources/views/maps/mapMobile.blade.php
@@ -3,8 +3,6 @@
 <head>
     <title>Mapa</title>
 
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <link href="https://fonts.googleapis.com/css?family=Nunito+Sans:300,400,600,700,800" rel="stylesheet">
 
     <!-- CSS here -->
@@ -49,20 +47,20 @@
     <link rel="stylesheet" href="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.css" />
     <script src="http://cdn.leafletjs.com/leaflet/v0.7.7/leaflet.js"></script>
     <style>
-        @media all and (max-width: 479px) {
-            #map{
-                width: 100%;
-                height: 100%;
-            }
-            #footer{
-                display: none;
-            }
+        body {
+            padding: 0;
+            margin: 0;
         }
-        #map { width: 100vw; height: 100vw; }
-        body { font: 16px/1.4 "Helvetica Neue", Arial, sans-serif; }
+        html, body, #map {
+            height: 100%;
+        }
+
         .ghbtns { position: relative; top: 4px; margin-left: 5px; }
         a { color: #0077ff; }
     </style>
+
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+
 </head>
 
 <body>
@@ -78,8 +76,7 @@
 
 
 <script>
-
-
+    
     var testData = {
         max: 8,
         data: [{lat: 38.926810, lng:-0.165582, count: 3},{lat: 38.996860, lng:-0.165532, count: 2},{lat: 38.996810, lng:-0.145582, count: 3},{lat: 38.996857, lng:-0.165588, count: 1},{lat: 38.996834, lng:-0.165533, count: 1},{lat: 60.8, lng:11.1, count: 1}]
@@ -135,6 +132,86 @@
     layer = heatmapLayer;
 
     L.control.layers(null, baseMaps).addTo(map);
+
+    /*
+    map.locate({setView: true, maxZoom: 16});
+
+    function onLocationFound(e) {
+        var radius = e.accuracy;
+
+        L.marker(e.latlng).addTo(map)
+            .bindPopup("You are within " + radius + " meters from this point").openPopup();
+
+        L.circle(e.latlng, radius).addTo(map);
+    }
+
+    map.on('locationfound', onLocationFound);
+
+    function onLocationError(e) {
+        alert(e.message);
+    }
+
+    map.on('locationerror', onLocationError);
+
+     */
+
+   /* var testData = {
+        max: 8,
+        data: [{lat: 38.926810, lng:-0.165582, count: 3},{lat: 38.996860, lng:-0.165532, count: 2},{lat: 38.996810, lng:-0.145582, count: 3},{lat: 38.996857, lng:-0.165588, count: 1},{lat: 38.996834, lng:-0.165533, count: 1},{lat: 60.8, lng:11.1, count: 1}]
+    };
+
+    var littleton = L.marker([38.996834, -0.165511]).bindPopup('This is Littleton, CO.'),
+        denver    = L.marker([38.996831, -0.165587]).bindPopup('This is Denver, CO.'),
+        aurora    = L.marker([38.996815, -0.165532]).bindPopup('This is Aurora, CO.'),
+        golden    = L.marker([38.996819, -0.165682]).bindPopup('This is Golden, CO.'),
+        walway = L.marker([38.996838, -0.165510]).bindPopup('This is Littleton, CO.'),
+        manises    = L.marker([38.996836, -0.165587]).bindPopup('This is Denver, CO.'),
+        xirivella    = L.marker([38.996825, -0.165512]).bindPopup('This is Aurora, CO.'),
+        bcristo    = L.marker([38.996818, -0.165682]).bindPopup('This is Golden, CO.');
+
+    var cities = L.layerGroup([littleton, denver, aurora, golden]);
+    var ciudades = L.layerGroup ([walway, manises, xirivella, bcristo]);
+    var baseMaps = {
+        "Cities": cities,
+        "Ciudades" : ciudades
+    };
+
+    var baseLayer = L.tileLayer(
+        'http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png',{
+            attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://cloudmade.com">CloudMade</a>',
+            maxZoom: 18
+        }
+    );
+    var cfg = {
+        // radius should be small ONLY if scaleRadius is true (or small radius is intended)
+        "radius": 0.002,
+        "maxOpacity": .8,
+        // scales the radius based on map zoom
+        "scaleRadius": true,
+        // if set to false the heatmap uses the global maximum for colorization
+        // if activated: uses the data maximum within the current map boundaries
+        //   (there will always be a red spot with useLocalExtremas true)
+        "useLocalExtrema": true,
+        // which field name in your data represents the latitude - default "lat"
+        latField: 'lat',
+        // which field name in your data represents the longitude - default "lng"
+        lngField: 'lng',
+        // which field name in your data represents the data value - default "value"
+        valueField: 'count'
+    };
+    var heatmapLayer = new HeatmapOverlay(cfg);
+    var map = new L.Map('map', {
+        center: new L.LatLng(38.996810, -0.165582),
+        zoom: 12,
+        layers: [baseLayer, heatmapLayer]
+    });
+    heatmapLayer.setData(testData);
+    // make accessible for debugging
+    layer = heatmapLayer;
+
+    L.control.layers(null, baseMaps).addTo(map);
+
+    */
 
 
 
