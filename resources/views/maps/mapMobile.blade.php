2fd8fafaa4c879e88f9a5e59909413d79784be62
Belén Grande <bgralop@epsg.upv.es>
2022-01-12 15:11:28 +0100

    El PUTO slider funcionando :)))))

--- resources/views/maps/mapMobile.blade.php
+++ resources/views/maps/mapMobile.blade.php
@@ -72,7 +72,6 @@
 <!-- <script src="../node_modules/simpleheat/simpleheat.js"></script>
 <script src="../src/HeatLayer.js"></script> -->
 
-<script src="../../../public/js/leaflet-heat.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/heatmapjs@2.0.2/heatmap.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/leaflet-heatmap@1.0.0/leaflet-heatmap.js"></script>
 
@@ -83,14 +82,213 @@
 <!-- Include this library for mobile touch support  -->
 <script src="http://cdnjs.cloudflare.com/ajax/libs/jqueryui-touch-punch/0.2.2/jquery.ui.touch-punch.min.js"></script>
 
-<script src="../../../public/js/SliderControl.js"></script>
+
+<!-- Script para que el slider funcione correctamente -->
+<script >
+
+    L.Control.SliderControl = L.Control.extend({
+        options: {
+            position: 'bottomright',
+            layers: null,
+            timeAttribute: 'time',
+            isEpoch: false,     // whether the time attribute is seconds elapsed from epoch
+            startTimeIdx: 0,    // where to start looking for a timestring
+            timeStrLength: 19,  // the size of  yyyy-mm-dd hh:mm:ss - if millis are present this will be larger
+            maxValue: -1,
+            minValue: 0,
+            showAllOnStart: false,
+            markers: null,
+            range: false,
+            follow: false,
+            sameDate: false,
+            alwaysShowDate : false,
+            rezoom: null
+        },
+
+        initialize: function (options) {
+            L.Util.setOptions(this, options);
+            this._layer = this.options.layer;
+
+        },
+
+        extractTimestamp: function(time, options) {
+            if (options.isEpoch) {
+                time = (new Date(parseInt(time))).toString(); // this is local time
+            }
+            return time.substr(options.startTimeIdx, options.startTimeIdx + options.timeStrLength);
+        },
+
+        setPosition: function (position) {
+            var map = this._map;
+
+            if (map) {
+                map.removeControl(this);
+            }
+
+            this.options.position = position;
+
+            if (map) {
+                map.addControl(this);
+            }
+            this.startSlider();
+            return this;
+        },
+
+        onAdd: function (map) {
+            this.options.map = map;
+
+            // Create a control sliderContainer with a jquery ui slider
+            var sliderContainer = L.DomUtil.create('div', 'slider', this._container);
+            $(sliderContainer).append('<div id="leaflet-slider" style="width:200px"><div class="ui-slider-handle"></div><div id="slider-timestamp" style="width:200px; margin-top:13px; background-color:#FFFFFF; text-align:center; border-radius:5px;"></div></div>');
+            //Prevent map panning/zooming while using the slider
+            $(sliderContainer).mousedown(function () {
+                map.dragging.disable();
+            });
+            $(document).mouseup(function () {
+                map.dragging.enable();
+                //Hide the slider timestamp if not range and option alwaysShowDate is set on false
+                if (options.range || !options.alwaysShowDate) {
+                    $('#slider-timestamp').html('');
+                }
+            });
+
+            var options = this.options;
+            this.options.markers = [];
+
+            //If a layer has been provided: calculate the min and max values for the slider
+            if (this._layer) {
+                var index_temp = 0;
+                this._layer.eachLayer(function (layer) {
+                    options.markers[index_temp] = layer;
+                    ++index_temp;
+                });
+                options.maxValue = index_temp - 1;
+                this.options = options;
+            } else {
+                console.log("Error: You have to specify a layer via new SliderControl({layer: your_layer});");
+            }
+            return sliderContainer;
+        },
+
+        onRemove: function (map) {
+            //Delete all markers which where added via the slider and remove the slider div
+            for (i = this.options.minValue; i <= this.options.maxValue; i++) {
+                map.removeLayer(this.options.markers[i]);
+            }
+            $('#leaflet-slider').remove();
+
+            // unbind listeners to prevent memory leaks
+            $(document).off("mouseup");
+            $(".slider").off("mousedown");
+        },
+
+        startSlider: function () {
+            _options = this.options;
+            _extractTimestamp = this.extractTimestamp
+            var index_start = _options.minValue;
+            if(_options.showAllOnStart){
+                index_start = _options.maxValue;
+                if(_options.range) _options.values = [_options.minValue,_options.maxValue];
+                else _options.value = _options.maxValue;
+            }
+            $("#leaflet-slider").slider({
+                range: _options.range,
+                value: _options.value,
+                values: _options.values,
+                min: _options.minValue,
+                max: _options.maxValue,
+                sameDate: _options.sameDate,
+                step: 1,
+                slide: function (e, ui) {
+                    var map = _options.map;
+                    var fg = L.featureGroup();
+                    if(!!_options.markers[ui.value]) {
+                        // If there is no time property, this line has to be removed (or exchanged with a different property)
+                        if(_options.markers[ui.value].feature !== undefined) {
+                            if(_options.markers[ui.value].feature.properties[_options.timeAttribute]){
+                                if(_options.markers[ui.value]) $('#slider-timestamp').html(
+                                    _extractTimestamp(_options.markers[ui.value].feature.properties[_options.timeAttribute], _options));
+                            }else {
+                                console.error("Time property "+ _options.timeAttribute +" not found in data");
+                            }
+                        }else {
+                            // set by leaflet Vector Layers
+                            if(_options.markers [ui.value].options[_options.timeAttribute]){
+                                if(_options.markers[ui.value]) $('#slider-timestamp').html(
+                                    _extractTimestamp(_options.markers[ui.value].options[_options.timeAttribute], _options));
+                            }else {
+                                console.error("Time property "+ _options.timeAttribute +" not found in data");
+                            }
+                        }
+
+                        var i;
+                        // clear markers
+                        for (i = _options.minValue; i <= _options.maxValue; i++) {
+                            if(_options.markers[i]) map.removeLayer(_options.markers[i]);
+                        }
+                        if(_options.range){
+                            // jquery ui using range
+                            for (i = ui.values[0]; i <= ui.values[1]; i++){
+                                if(_options.markers[i]) {
+                                    map.addLayer(_options.markers[i]);
+                                    fg.addLayer(_options.markers[i]);
+                                }
+                            }
+                        }else if(_options.follow){
+                            for (i = ui.value - _options.follow + 1; i <= ui.value ; i++) {
+                                if(_options.markers[i]) {
+                                    map.addLayer(_options.markers[i]);
+                                    fg.addLayer(_options.markers[i]);
+                                }
+                            }
+                        }else if(_options.sameDate){
+                            var currentTime;
+                            if (_options.markers[ui.value].feature !== undefined) {
+                                currentTime = _options.markers[ui.value].feature.properties.time;
+                            } else {
+                                currentTime = _options.markers[ui.value].options.time;
+                            }
+                            for (i = _options.minValue; i <= _options.maxValue; i++) {
+                                if(_options.markers[i].options.time == currentTime) map.addLayer(_options.markers[i]);
+                            }
+                        }else{
+                            for (i = _options.minValue; i <= ui.value ; i++) {
+                                if(_options.markers[i]) {
+                                    map.addLayer(_options.markers[i]);
+                                    fg.addLayer(_options.markers[i]);
+                                }
+                            }
+                        }
+                    };
+                    if(_options.rezoom) {
+                        map.fitBounds(fg.getBounds(), {
+                            maxZoom: _options.rezoom
+                        });
+                    }
+                }
+            });
+            if (!_options.range && _options.alwaysShowDate) {
+                $('#slider-timestamp').html(_extractTimeStamp(_options.markers[index_start].feature.properties[_options.timeAttribute], _options));
+            }
+            for (i = _options.minValue; i <= index_start; i++) {
+                _options.map.addLayer(_options.markers[i]);
+            }
+        }
+    });
+
+    L.control.sliderControl = function (options) {
+        return new L.Control.SliderControl(options);
+    };
 
 
-<script src="https://github.com/dwilhelm89/LeafletSlider/blob/a797b08d692b9c9deaf5602ebeb99923d8764f5e/SliderControl.js"></script>
+</script>
+
+
 
 <script>
 
 
+    // Iconos personalizados
     var LeafIcon = L.Icon.extend({
         options: {
             iconSize:     [38, 95],
@@ -101,6 +299,11 @@
         }
     });
 
+    const fontAwesomeIcon = L.divIcon({
+        html: '<i class="fa fa-map-marker fa-4x"></i>',
+        iconSize: [20, 20],
+        className: 'myDivIcon'
+    });
 
     var greenIcon = new LeafIcon({
         iconUrl: 'http://leafletjs.com/examples/custom-icons/leaf-green.png',
@@ -109,6 +312,8 @@
 
 
     var sliderControl = null;
+
+
     /**
      * Descripción de obtenerMedicionDeLasEstaciones. Función que devuelve la medicion de las estaciones oficiales de medida
      * Haciendo una petición a la api oficial
@@ -127,14 +332,14 @@
     }
     var medu=obtenerMedicionDeLasEstaciones(40.136944, 0.165555);
 
-    var estacionPrat = L.marker([40.136944, 0.165555], {icon: greenIcon}).bindPopup('Estacion de Prat de Cabanes: '+  medu),
+    var estacionPrat = L.marker([40.136944, 0.165555], {icon: fontAwesomeIcon}).bindPopup('Estacion de Prat de Cabanes: '+  medu),
         estacionDenia = L.marker([38.67194, 0.0358333]).bindPopup('Estacion de Dénia: '+ obtenerMedicionDeLasEstaciones(38.67194, 0.0358333)),
         estacionAras = L.marker([39.950277, -1.108888]).bindPopup('Estacion de Aras de los olmos:' + obtenerMedicionDeLasEstaciones(39.950277, -1.108888)),
         estacionValencia = L.marker([39.950277, -0.035833]).bindPopup('Estacion de Valencia: '+ obtenerMedicionDeLasEstaciones(39.950277, -0.035833)),
         estacionTorrevieja = L.marker([38.00833, -0.658611]).bindPopup('Estacion de Torrevieja: '+ obtenerMedicionDeLasEstaciones(38.00833, -0.658611));
 
 
-
+/*
     var marker1 = L.marker([38.996836, -0.165513], {time: "2013-01-22 08:42:26+01"});
     var marker2 = L.marker([38.996838, -0.165510], {time: "2013-01-22 10:00:26+01"});
     var marker3 = L.marker([38.996823, -0.165545], {time: "2013-01-22 10:03:29+01"});
@@ -151,26 +356,39 @@
         smoothFactor: 1
     });
 
+     */
+
     var testData = {
         max: 8,
         data: [{lat: 38.926810, lng:-0.165582, count: 3},{lat: 38.996860, lng:-0.165532, count: 2},{lat: 38.996810, lng:-0.145582, count: 3},{lat: 38.996857, lng:-0.165588, count: 1},{lat: 38.996834, lng:-0.165533, count: 1},{lat: 60.8, lng:11.1, count: 1}]
     };
 
-    var medicionIAQ1 = L.marker([38.996834, -0.165511]).bindPopup('La calidad del aire en este punto es buena'),
-        medicionIAQ2    = L.marker([38.996831, -0.165587]).bindPopup('La calidad del aire en este punto es buena'),
-        medicionIAQ3    = L.marker([38.996815, -0.165532]).bindPopup('La calidad del aire en este punto es buena'),
-        medicionIAQ4    = L.marker([38.996819, -0.165682]).bindPopup('La calidad del aire en este punto es buena'),
-        medicionIAQ5    = L.marker([38.996838, -0.178382]).bindPopup('La calidad del aire en este punto es normal'),
-        medicionIAQ6    = L.marker([38.996719, -0.145682]).bindPopup('La calidad del aire en este punto es buena'),
-        medicionIAQ7    = L.marker([38.993819, -0.185682]).bindPopup('La calidad del aire en este punto es mala'),
-        medicionIAQ8    = L.marker([38.996813, -0.175682]).bindPopup('La calidad del aire en este punto es buena'),
-        medicionIAQ9    = L.marker([38.996819, -0.175562]).bindPopup('La calidad del aire en este punto es normal'),
-        medicionIAQ10    = L.marker([38.976819, -0.165212]).bindPopup('La calidad del aire en este punto es mala'),
-        medicionIAQ11   = L.marker([38.986819, -0.165562]).bindPopup('La calidad del aire en este punto es buena'),
-        medicionIAQ12    = L.marker([38.992229, -0.165342]).bindPopup('La calidad del aire en este punto es buena'),
-        medicionIAQ13    = L.marker([38.996219, -0.165634]).bindPopup('La calidad del aire en este punto es buena'),
-        medicionIAQ14    = L.marker([38.996349, -0.182682]).bindPopup('La calidad del aire en este punto es buena'),
-        medicionIAQ15    = L.marker([38.996763, -0.177682]).bindPopup('La calidad del aire en este punto es mala'),
+    var medicionIAQ1 = L.marker([38.966623, -0.175211], {time: "2013-01-22 09:42:26+01"}).bindPopup('La calidad del aire en este punto es a'),
+        medicionIAQ2    = L.marker([38.992851, -0.195587], {time: "2013-01-22 09:42:26+01"}).bindPopup('La calidad del aire en este punto es b'),
+        medicionIAQ3    = L.marker([38.991045, -0.165532], {time: "2013-01-22 10:42:26+01"}).bindPopup('La calidad del aire en este punto es c'),
+        medicionIAQ4    = L.marker([38.994319, -0.161632], {time: "2013-01-22 11:42:26+01"}).bindPopup('La calidad del aire en este punto es d'),
+        medicionIAQ5    = L.marker([38.986838, -0.178382], {time: "2013-01-22 15:00:26+01"}).bindPopup('La calidad del aire en este punto es e'),
+        medicionIAQ6    = L.marker([38.986419, -0.165682], {time: "2013-01-22 18:00:26+01"}).bindPopup('La calidad del aire en este punto es f'),
+        medicionIAQ7    = L.marker([38.983519, -0.185682], {time: "2013-01-22 18:00:26+01"}).bindPopup('La calidad del aire en este punto es g'),
+        medicionIAQ8    = L.marker([38.996813, -0.175682], {time: "2013-01-22 18:00:26+01"}).bindPopup('La calidad del aire en este punto es h'),
+        medicionIAQ9    = L.marker([38.956719, -0.175562], {time: "2013-01-22 19:00:26+01"}).bindPopup('La calidad del aire en este punto es i'),
+        medicionIAQ10    = L.marker([38.976829, -0.175212], {time: "2013-01-22 20:00:26+01"}).bindPopup('La calidad del aire en este punto es j'),
+        medicionIAQ11   = L.marker([38.986859, -0.168862], {time: "2013-01-22 21:03:29+01"}).bindPopup('La calidad del aire en este punto es k'),
+        medicionIAQ12    = L.marker([38.992229, -0.175342], {time: "2013-01-22 22:03:29+01"}).bindPopup('La calidad del aire en este punto es l'),
+        medicionIAQ13    = L.marker([38.984544, -0.165634], {time: "2013-01-22 23:03:29+01"}).bindPopup('La calidad del aire en este punto es m'),
+        medicionIAQ14    = L.marker([38.976349, -0.182682], {time: "2013-01-22 23:03:29+01"}).bindPopup('La calidad del aire en este punto es n'),
+        medicionIAQ15    = L.marker([38.998223, -0.177682], {time: "2013-01-22 23:03:29+01"}).bindPopup('La calidad del aire en este punto es o'),
+        medicionIAQ16    = L.marker([38.998224, -0.177683], {time: "2013-01-22 23:03:29+01"}).bindPopup('La calidad del aire en este punto es p'),
+        medicionIAQ17    = L.marker([38.988243, -0.177682], {time: "2013-01-23 08:03:29+01"}).bindPopup('La calidad del aire en este punto es q'),
+        medicionIAQ18    = L.marker([38.789987, -0.189682], {time: "2013-01-23 09:03:29+01"}).bindPopup('La calidad del aire en este punto es r'),
+        medicionIAQ19    = L.marker([38.994423, -0.177682], {time: "2013-01-23 10:03:29+01"}).bindPopup('La calidad del aire en este punto es s'),
+        medicionIAQ20    = L.marker([38.986723, -0.174682], {time: "2013-01-23 12:03:29+01"}).bindPopup('La calidad del aire en este punto es t'),
+        medicionIAQ21    = L.marker([38.994423, -0.165434], {time: "2013-01-23 14:03:29+01"}).bindPopup('La calidad del aire en este punto es u'),
+        medicionIAQ22    = L.marker([38.893223, -0.167682], {time: "2013-01-23 17:03:29+01"}).bindPopup('La calidad del aire en este punto es v'),
+        medicionIAQ23    = L.marker([38.988623, -0.178942], {time: "2013-01-23 19:03:29+01"}).bindPopup('La calidad del aire en este punto es w'),
+        medicionIAQ24    = L.marker([38.996723, -0.156632], {time: "2013-01-23 20:03:29+01"}).bindPopup('La calidad del aire en este punto es x'),
+        medicionIAQ25    = L.marker([38.993423, -0.176341], {time: "2013-01-23 22:03:29+01"}).bindPopup('La calidad del aire en este punto es y'),
+        medicionIAQ26    = L.marker([38.998345, -0.177731], {time: "2013-01-23 22:03:29+01"}).bindPopup('La calidad del aire en este punto es z'),
 
 
         medicionSO21 = L.marker([38.996838, -0.165510]).bindPopup('El valor de SO2 es: 1'),
@@ -220,6 +438,7 @@
         // which field name in your data represents the data value - default "value"
         valueField: 'count'
     };
+
     var heatmapLayer = new HeatmapOverlay(cfg);
 
 
@@ -230,16 +449,11 @@
         zoom: 12,
         layers: [baseLayer,heatmapLayer]
 
-
     });
-
-
-    layerGroup = L.layerGroup([marker1, marker2, marker3, polyline]);
-
 
     L.control.layers(null, baseMaps).addTo(map);
 
-    L.control.sliderControl({position: "bottomright", layer: layerGroup}).addTo(map);
+    var sliderControl = L.control.sliderControl({position: "bottomright", layer: IAQ});
     map.addControl(sliderControl);
     sliderControl.startSlider();
 
@@ -248,7 +462,7 @@
     // make accessible for debugging
     layer = heatmapLayer;
 
-    
+
     /**
      * Descripción de obtenerMedicionDeLasEstaciones. Función que devuelve la medicion de las estaciones oficiales de medida
      * Haciendo una petición a la api oficial
