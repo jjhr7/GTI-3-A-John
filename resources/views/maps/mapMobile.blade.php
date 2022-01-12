4028203ac212f5ec4884d3ebbefc4c94d436e876
Belén Grande <bgralop@epsg.upv.es>
2022-01-12 17:34:18 +0100

    putos marcadores cambiados

--- resources/views/maps/mapMobile.blade.php
+++ resources/views/maps/mapMobile.blade.php
@@ -383,21 +383,48 @@
             className: 'myDivIcon'
         });
 
+        var estacionesIcon = new L.Icon({
+            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-green.png',
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        });
+
+        var COIcon = new L.Icon({
+            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-violet.png',
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        });
+
+        var SOIcon = new L.Icon({
+            iconUrl: 'https://raw.githubusercontent.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-yellow.png',
+            shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/0.7.7/images/marker-shadow.png',
+            iconSize: [25, 41],
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34],
+            shadowSize: [41, 41]
+        });
 
 
-        var estacionPrat = L.marker([40.136944, 0.165555]).bindPopup('Estacion de Prat de Cabanes:' + '\r' + ' Humedad: ' + prat.weather.hu + "\r" +
+
+        var estacionPrat = L.marker([40.136944, 0.165555], {icon: estacionesIcon}).bindPopup('Estacion de Prat de Cabanes:' + '\r' + ' Humedad: ' + prat.weather.hu + "\r" +
                 'Temperatura: ' + prat.weather.tp + "\r" +
                 'Calidad del aire: ' + prat.pollution.aqicn + "\r"),
-            estacionDenia = L.marker([38.67194, 0.0358333]).bindPopup('Estacion de Dénia: ' + '\r' + ' Humedad: ' + denia.weather.hu + "\r" +
+            estacionDenia = L.marker([38.67194, 0.0358333],  {icon: estacionesIcon}).bindPopup('Estacion de Dénia: ' + '\r' + ' Humedad: ' + denia.weather.hu + "\r" +
                 'Temperatura: ' + denia.weather.tp + "\r" +
                 'Calidad del aire: ' + denia.pollution.aqicn + "\r"),
-            estacionAras = L.marker([39.950277, -1.108888]).bindPopup('Estacion de Aras de los olmos:' + '\r' + ' Humedad: ' + aras.weather.hu + "\r" +
+            estacionAras = L.marker([39.950277, -1.108888], {icon: estacionesIcon}).bindPopup('Estacion de Aras de los olmos:' + '\r' + ' Humedad: ' + aras.weather.hu + "\r" +
                 'Temperatura: ' + aras.weather.tp + "\r" +
                 'Calidad del aire: ' + aras.pollution.aqicn + "\r"),
-            estacionValencia = L.marker([39.950277, -0.035833]).bindPopup('Estacion de Valencia: ' + '\r' + ' Humedad: ' + valencia.weather.hu + "\r" +
+            estacionValencia = L.marker([39.950277, -0.035833], {icon: estacionesIcon}).bindPopup('Estacion de Valencia: ' + '\r' + ' Humedad: ' + valencia.weather.hu + "\r" +
                 'Temperatura: ' + valencia.weather.tp + "\r" +
                 'Calidad del aire: ' + valencia.pollution.aqicn + "\r"),
-            estacionTorrevieja = L.marker([38.00833, -0.658611]).bindPopup('Estacion de Torrevieja: ' + '\r' + ' Humedad: ' + torrevieja.weather.hu + "\r" +
+            estacionTorrevieja = L.marker([38.00833, -0.658611], {icon: estacionesIcon}).bindPopup('Estacion de Torrevieja: ' + '\r' + ' Humedad: ' + torrevieja.weather.hu + "\r" +
                 'Temperatura: ' + torrevieja.weather.tp + "\r" +
                 'Calidad del aire: ' + torrevieja.pollution.aqicn + "\r");
 
@@ -430,48 +457,49 @@
             data: [{lat: 38.926810, lng:-0.165582, count: 3},{lat: 38.996860, lng:-0.165532, count: 2},{lat: 38.996810, lng:-0.145582, count: 3},{lat: 38.996857, lng:-0.165588, count: 1},{lat: 38.996834, lng:-0.165533, count: 1},{lat: 60.8, lng:11.1, count: 1}]
         };
 
-        var medicionIAQ1 = L.marker([38.966623, -0.175211], {time: "2013-01-22 09:42:26+01"}).bindPopup('La calidad del aire en este punto es a'),
-            medicionIAQ2    = L.marker([38.992851, -0.195587], {time: "2013-01-22 09:42:26+01"}).bindPopup('La calidad del aire en este punto es b'),
-            medicionIAQ3    = L.marker([38.991045, -0.165532], {time: "2013-01-22 10:42:26+01"}).bindPopup('La calidad del aire en este punto es c'),
-            medicionIAQ4    = L.marker([38.994319, -0.161632], {time: "2013-01-22 11:42:26+01"}).bindPopup('La calidad del aire en este punto es d'),
-            medicionIAQ5    = L.marker([38.986838, -0.178382], {time: "2013-01-22 15:00:26+01"}).bindPopup('La calidad del aire en este punto es e'),
-            medicionIAQ6    = L.marker([38.986419, -0.165682], {time: "2013-01-22 18:00:26+01"}).bindPopup('La calidad del aire en este punto es f'),
-            medicionIAQ7    = L.marker([38.983519, -0.185682], {time: "2013-01-22 18:00:26+01"}).bindPopup('La calidad del aire en este punto es g'),
-            medicionIAQ8    = L.marker([38.996813, -0.175682], {time: "2013-01-22 18:00:26+01"}).bindPopup('La calidad del aire en este punto es h'),
-            medicionIAQ9    = L.marker([38.956719, -0.175562], {time: "2013-01-22 19:00:26+01"}).bindPopup('La calidad del aire en este punto es i'),
-            medicionIAQ10    = L.marker([38.976829, -0.175212], {time: "2013-01-22 20:00:26+01"}).bindPopup('La calidad del aire en este punto es j'),
-            medicionIAQ11   = L.marker([38.986859, -0.168862], {time: "2013-01-22 21:03:29+01"}).bindPopup('La calidad del aire en este punto es k'),
-            medicionIAQ12    = L.marker([38.992229, -0.175342], {time: "2013-01-22 22:03:29+01"}).bindPopup('La calidad del aire en este punto es l'),
-            medicionIAQ13    = L.marker([38.984544, -0.165634], {time: "2013-01-22 23:03:29+01"}).bindPopup('La calidad del aire en este punto es m'),
-            medicionIAQ14    = L.marker([38.976349, -0.182682], {time: "2013-01-22 23:03:29+01"}).bindPopup('La calidad del aire en este punto es n'),
-            medicionIAQ15    = L.marker([38.998223, -0.177682], {time: "2013-01-22 23:03:29+01"}).bindPopup('La calidad del aire en este punto es o'),
-            medicionIAQ16    = L.marker([38.998224, -0.177683], {time: "2013-01-22 23:03:29+01"}).bindPopup('La calidad del aire en este punto es p'),
-            medicionIAQ17    = L.marker([38.988243, -0.177682], {time: "2013-01-23 08:03:29+01"}).bindPopup('La calidad del aire en este punto es q'),
-            medicionIAQ18    = L.marker([38.789987, -0.189682], {time: "2013-01-23 09:03:29+01"}).bindPopup('La calidad del aire en este punto es r'),
-            medicionIAQ19    = L.marker([38.994423, -0.177682], {time: "2013-01-23 10:03:29+01"}).bindPopup('La calidad del aire en este punto es s'),
-            medicionIAQ20    = L.marker([38.986723, -0.174682], {time: "2013-01-23 12:03:29+01"}).bindPopup('La calidad del aire en este punto es t'),
-            medicionIAQ21    = L.marker([38.994423, -0.165434], {time: "2013-01-23 14:03:29+01"}).bindPopup('La calidad del aire en este punto es u'),
-            medicionIAQ22    = L.marker([38.893223, -0.167682], {time: "2013-01-23 17:03:29+01"}).bindPopup('La calidad del aire en este punto es v'),
-            medicionIAQ23    = L.marker([38.988623, -0.178942], {time: "2013-01-23 19:03:29+01"}).bindPopup('La calidad del aire en este punto es w'),
-            medicionIAQ24    = L.marker([38.996723, -0.156632], {time: "2013-01-23 20:03:29+01"}).bindPopup('La calidad del aire en este punto es x'),
-            medicionIAQ25    = L.marker([38.993423, -0.176341], {time: "2013-01-23 22:03:29+01"}).bindPopup('La calidad del aire en este punto es y'),
-            medicionIAQ26    = L.marker([38.998345, -0.177731], {time: "2013-01-23 22:03:29+01"}).bindPopup('La calidad del aire en este punto es z'),
+        var medicionIAQ1 = L.marker([38.966623, -0.175211], {time: "2013-01-22 07:42:26"}).bindPopup('La calidad del aire en este punto es a'),
+            medicionIAQ2    = L.marker([38.992851, -0.195587], {time: "2013-01-22 07:42:26"}).bindPopup('La calidad del aire en este punto es b'),
+            medicionIAQ3    = L.marker([38.991045, -0.165532], {time: "2013-01-22 08:42:26"}).bindPopup('La calidad del aire en este punto es c'),
+            medicionIAQ4    = L.marker([38.994319, -0.161632], {time: "2013-01-22 08:42:26"}).bindPopup('La calidad del aire en este punto es d'),
+            medicionIAQ5    = L.marker([38.956838, -0.178382], {time: "2013-01-22 09:00:26"}).bindPopup('La calidad del aire en este punto es e'),
+            medicionIAQ6    = L.marker([38.986419, -0.165682], {time: "2013-01-22 10:00:26"}).bindPopup('La calidad del aire en este punto es f'),
+            medicionIAQ7    = L.marker([38.983519, -0.185682], {time: "2013-01-22 11:00:26"}).bindPopup('La calidad del aire en este punto es g'),
+            medicionIAQ8    = L.marker([38.996813, -0.175682], {time: "2013-01-22 11:06:26"}).bindPopup('La calidad del aire en este punto es h'),
+            medicionIAQ9    = L.marker([38.956719, -0.175562], {time: "2013-01-22 11:10:26"}).bindPopup('La calidad del aire en este punto es i'),
+            medicionIAQ10    = L.marker([38.976829, -0.175212], {time: "2013-01-22 11:20:26"}).bindPopup('La calidad del aire en este punto es j'),
+            medicionIAQ11   = L.marker([38.986859, -0.168862], {time: "2013-01-22 11:30:29"}).bindPopup('La calidad del aire en este punto es k'),
+            medicionIAQ12    = L.marker([38.992229, -0.175342], {time: "2013-01-22 12:03:29"}).bindPopup('La calidad del aire en este punto es l'),
+            medicionIAQ13    = L.marker([38.984544, -0.165634], {time: "2013-01-22 12:23:29"}).bindPopup('La calidad del aire en este punto es m'),
+            medicionIAQ14    = L.marker([38.976349, -0.182682], {time: "2013-01-22 12:29:29"}).bindPopup('La calidad del aire en este punto es n'),
+            medicionIAQ15    = L.marker([38.968223, -0.177682], {time: "2013-01-22 12:37:29"}).bindPopup('La calidad del aire en este punto es o'),
+            medicionIAQ16    = L.marker([38.998224, -0.177683], {time: "2013-01-22 12:55:29"}).bindPopup('La calidad del aire en este punto es p'),
+            medicionIAQ17    = L.marker([38.988243, -0.177682], {time: "2013-01-23 13:03:29"}).bindPopup('La calidad del aire en este punto es q'),
+            medicionIAQ18    = L.marker([38.789987, -0.189682], {time: "2013-01-23 13:30:29"}).bindPopup('La calidad del aire en este punto es r'),
+            medicionIAQ19    = L.marker([38.954423, -0.177682], {time: "2013-01-23 14:03:29"}).bindPopup('La calidad del aire en este punto es s'),
+            medicionIAQ20    = L.marker([38.969280, -0.187316], {time: "2013-01-23 15:03:29"}).bindPopup('La calidad del aire en este punto es t'),
+            medicionIAQ21    = L.marker([38.994423, -0.165434], {time: "2013-01-23 16:03:29"}).bindPopup('La calidad del aire en este punto es u'),
+            medicionIAQ22    = L.marker([38.893223, -0.167682], {time: "2013-01-23 16:03:29"}).bindPopup('La calidad del aire en este punto es v'),
+            medicionIAQ23    = L.marker([38.988623, -0.178942], {time: "2013-01-23 16:30:29"}).bindPopup('La calidad del aire en este punto es w'),
+            medicionIAQ24    = L.marker([38.996723, -0.156632], {time: "2013-01-23 18:03:29"}).bindPopup('La calidad del aire en este punto es x'),
+            medicionIAQ25    = L.marker([38.993423, -0.176341], {time: "2013-01-23 19:03:29"}).bindPopup('La calidad del aire en este punto es y'),
+            medicionIAQ26    = L.marker([38.979880, -0.189316], {time: "2013-01-23 20:03:29"}).bindPopup('La calidad del aire en este punto es z'),
 
 
-            medicionSO21 = L.marker([38.996838, -0.165510]).bindPopup('El valor de SO2 es: 1'),
-            medicionSO22    = L.marker([38.896833, -0.165514]).bindPopup('El valor de SO2 es: 2'),
-            medicionSO23    = L.marker([38.966523, -0.165420]).bindPopup('El valor de SO2 es: 3'),
-            medicionSO24    = L.marker([38.984544, -0.165720]).bindPopup('El valor de SO2 es: 5'),
-            medicionSO25    = L.marker([38.987619, -0.165827]).bindPopup('El valor de SO2 es: 4'),
-            medicionSO26    = L.marker([38.972356, -0.165332]).bindPopup('El valor de SO2 es: 6'),
-            medicionSO27    = L.marker([38.899833, -0.166682]).bindPopup('El valor de SO2 es: 7'),
-            medicionSO28    = L.marker([38.936128, -0.167882]).bindPopup('El valor de SO2 es: 8'),
-            medicionSO29    = L.marker([38.924763, -0.166542]).bindPopup('El valor de SO2 es: 9'),
-            medicionSO210    = L.marker([38.992318, -0.163282]).bindPopup('El valor de SO2 es: '),
-            medicionSO211    = L.marker([38.976118, -0.163342]).bindPopup('El valor de SO2 es: '),
-            medicionSO12    = L.marker([38.989818, -0.166642]).bindPopup('El valor de SO2 es: ');
+            medicionSO21 = L.marker([38.996838, -0.165510], {icon: SOIcon},{time: "2013-01-22 11:30:29"}).bindPopup('El valor de SO2 es: 1'),
+            medicionSO22    = L.marker([38.896833, -0.165514], {icon: SOIcon}, {time: "2013-01-22 12:30:29"}).bindPopup('El valor de SO2 es: 2'),
+            medicionSO23    = L.marker([38.966523, -0.165420], {icon: SOIcon}, {time: "2013-01-22 13:30:29"}).bindPopup('El valor de SO2 es: 3'),
+            medicionSO24    = L.marker([38.984544, -0.165720], {icon: SOIcon}, {time: "2013-01-22 14:30:29"}).bindPopup('El valor de SO2 es: 5'),
+            medicionSO25    = L.marker([38.987619, -0.165827], {icon: SOIcon}, {time: "2013-01-22 15:30:29"}).bindPopup('El valor de SO2 es: 4'),
+            medicionSO26    = L.marker([38.972356, -0.165332], {icon: SOIcon}, {time: "2013-01-22 16:30:29"}).bindPopup('El valor de SO2 es: 6'),
+            medicionSO27    = L.marker([38.899833, -0.166682], {icon: SOIcon}, {time: "2013-01-22 17:30:29"}).bindPopup('El valor de SO2 es: 7'),
+            medicionSO28    = L.marker([38.936128, -0.167882], {icon: SOIcon}, {time: "2013-01-22 18:30:29"}).bindPopup('El valor de SO2 es: 8'),
+            medicionSO29    = L.marker([38.924763, -0.166542], {icon: SOIcon}, {time: "2013-01-22 19:30:29"}).bindPopup('El valor de SO2 es: 9'),
+            medicionSO210    = L.marker([38.992318, -0.163282], {icon: SOIcon}, {time: "2013-01-22 20:30:29"}).bindPopup('El valor de SO2 es: '),
+            medicionSO211    = L.marker([38.976118, -0.163342], {icon: SOIcon}, {time: "2013-01-22 21:30:29"}).bindPopup('El valor de SO2 es: '),
+            medicionSO12    = L.marker([38.989818, -0.166642], {icon: SOIcon}, {time: "2013-01-22 22:30:29"}).bindPopup('El valor de SO2 es: ');
 
-        var IAQ = L.layerGroup([medicionIAQ1, medicionIAQ2, medicionIAQ3, medicionIAQ4, medicionIAQ5, medicionIAQ6,medicionIAQ7, medicionIAQ8, medicionIAQ9, medicionIAQ10, medicionIAQ11, medicionIAQ12, medicionIAQ13, medicionIAQ14, medicionIAQ15]);
+
+        var IAQ = L.layerGroup([medicionIAQ1, medicionIAQ2, medicionIAQ3, medicionIAQ4, medicionIAQ5, medicionIAQ6,medicionIAQ7, medicionIAQ8, medicionIAQ9, medicionIAQ10, medicionIAQ11, medicionIAQ12, medicionIAQ13, medicionIAQ14, medicionIAQ15, medicionIAQ16, medicionIAQ17, medicionIAQ18, medicionIAQ19, medicionIAQ20, medicionIAQ21, medicionIAQ22, medicionIAQ23, medicionIAQ24,medicionIAQ25, medicionIAQ26]);
         var SO2 = L.layerGroup ([medicionSO21, medicionSO22, medicionSO23, medicionSO24, medicionSO25, medicionSO26, medicionSO27, medicionSO28,medicionSO29,medicionSO210,medicionSO211, medicionSO12]);
         var estacionesMedica = L.layerGroup ([estacionDenia, estacionPrat, estacionValencia, estacionAras, estacionTorrevieja]);
         var baseMaps = {
